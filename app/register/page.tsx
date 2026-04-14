1ccb4c36189ecaf1b95e1c310a7cba6f886ed42f
TAHIYA NUZHATH KHAN <tahiya@graduate.utm.my>
2026-04-14 17:49:26 +0800

    Auth UI with TypeScript, Tailwind, Lucide

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { Mail, Lock, User, Eye, EyeOff } from "lucide-react";
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
